cdd0d3cc8dd28ba2ca56472dc6f344c47cc7d355
saurabhjn76 <saurabhjn76@gmail.com>
2016-01-25 13:12:52 +0530

    "alpha release"

--- SunPathDiagram/app/src/main/java/iiitvstudents/com/sunpathdiagram/InputActivity.java
+++ SunPathDiagram/app/src/main/java/iiitvstudents/com/sunpathdiagram/InputActivity.java
@@ -1,35 +1,181 @@
 package iiitvstudents.com.sunpathdiagram;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.location.*;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Calendar;
 
 public class InputActivity extends AppCompatActivity {
-    public static final String LATITUDE="iiitvstudents.com.sunpathdidagram.latitudemessage";
-    public static final String DAYNUMBER="iiitvstudents.com.sunpathdidagram.daynumber";
+    public static final String LATITUDE = "iiitvstudents.com.sunpathdidagram.latitudemessage";
+    public static final String DAYNUMBER = "iiitvstudents.com.sunpathdidagram.daynumber";
+    static int num;
+    static EditText dayNumberText;
+    static EditText latitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-        final EditText latitudeText=(EditText)findViewById(R.id.latitude);
-        final EditText dayNumberText=(EditText)findViewById(R.id.daynumber);
+        latitudeText = (EditText) findViewById(R.id.latitude);
+        dayNumberText = (EditText) findViewById(R.id.daynumber);
+        ImageView getlocation = (ImageView) findViewById(R.id.locationButton);
+        getlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               final LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+                LocationListener locationListener = new LocationListener() {
+                    public void onLocationChanged(Location location) {
+                        Log.e("im","here");
+                        // Called when a new location is found by the network location provider.
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        Button button=(Button)findViewById(R.id.button);
+                            return;
+                        }
+                        Location l = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                        double latitude=l.getLatitude();
+                        Log.e("loc",""+latitude);
+                        latitudeText.setText(""+latitude);
+
+                    }
+
+                    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                    public void onProviderEnabled(String provider) {}
+
+                    public void onProviderDisabled(String provider) {}
+                };
+
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    }
+                    Log.e("sfas","SAfas");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100000, 1000, locationListener);
+                    Location l = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    double latitude=l.getLatitude();
+                    Log.e("loc",""+latitude);
+                    latitudeText.setText("" + latitude);
+
+                }
+                else {
+                    IssueDialog issueDialog =new IssueDialog();
+                    issueDialog.show(getFragmentManager(),"Location");
+                }
+            }
+        });
+
+// Register the listener with the Location Manager to receive location updates
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent plotActivity = new Intent(getApplicationContext(), PlotActivity.class);
-                double latitudeValue = Double.parseDouble(latitudeText.getText().toString());
-                int dayNumberValue = Integer.parseInt(dayNumberText.getText().toString());
-                plotActivity.putExtra(LATITUDE, latitudeValue);
-                plotActivity.putExtra(DAYNUMBER, dayNumberValue);
-                startActivity(plotActivity);
+                double latitudeValue;
+                try
+                {latitudeValue = Double.parseDouble(latitudeText.getText().toString());
+
+                if (latitudeValue<90&&latitudeValue>-90){
+                    int dayNumberValue = Integer.parseInt(dayNumberText.getText().toString());
+                    if(dayNumberValue<365&&dayNumberValue>0) {
+                        plotActivity.putExtra(LATITUDE, latitudeValue);
+                        plotActivity.putExtra(DAYNUMBER, dayNumberValue);
+                        startActivity(plotActivity);
+                    }
+                    else {
+                        Snackbar.make(v,"Number of days must be between 0 to 365",Snackbar.LENGTH_INDEFINITE).show();
+                    }
+                }else {
+                    Snackbar.make(v,"Latitude must be between -90 to 90",Snackbar.LENGTH_INDEFINITE).show();
+                }}
+                catch (NumberFormatException e){Snackbar.make(v, "Invalid Input", Snackbar.LENGTH_INDEFINITE).show();}
             }
         });
+    }
+    public void datepick(View view){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
 
+    }
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            DatePicker datePicker= view;
+            Calendar cal=Calendar.getInstance();
+            cal.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
+            int month_=datePicker.getMonth();
+            int date=datePicker.getDayOfMonth();
+            int number;
+            int mnth=1;
+             num=0;
+            while(mnth<month_) {
+                if (mnth == 1 || mnth == 3 || mnth == 5 || mnth == 7 || mnth == 8 || mnth == 10 || mnth == 12) {
+                    num += 31;
+                } else if (mnth == 2) {
+                    num += 28;
+                } else {
+                    num += 30;
+                }
+                mnth++;
+            }
+            num+=date;
+            dayNumberText.setText(""+num);
+        }
+
+    }
+    public static class  IssueDialog extends android.app.DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("GPS is not Active")
+                    .setPositiveButton("Turn On", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent locationSetting=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(locationSetting);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
     }
 }
--- SunPathDiagram/app/src/main/java/iiitvstudents/com/sunpathdiagram/PlotActivity.java
+++ SunPathDiagram/app/src/main/java/iiitvstudents/com/sunpathdiagram/PlotActivity.java
@@ -1,62 +1,172 @@
 package iiitvstudents.com.sunpathdiagram;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.Size;
+import android.view.MotionEvent;
+import android.view.View;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 
-import com.androidplot.xy.CatmullRomInterpolator;
+
+import com.androidplot.ui.AnchorPosition;
+import com.androidplot.ui.DynamicTableModel;
+import com.androidplot.ui.SizeLayoutType;
+import com.androidplot.ui.XLayoutStyle;
+import com.androidplot.ui.YLayoutStyle;
+import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
 import com.androidplot.xy.XYStepMode;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class PlotActivity extends AppCompatActivity {
-XYPlot sunpathplot;
+    XYPlot sunpathplot;
+    PointF maxXY;
+    PointF minXY;
+    float zoomRatio = 2, leftBoundary, rightBoundary;
+    SimpleXYSeries series;
+    SimpleXYSeries series_summersolstice;
+    SimpleXYSeries series_eqinox;
+    SimpleXYSeries series_wintersolstice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plot);
         sunpathplot=(XYPlot)findViewById(R.id.plot);
+//        sunpathplot.setOnTouchListener(this);
         Intent from=getIntent();
         Double latitudeValue=from.getDoubleExtra(InputActivity.LATITUDE, 0);
         int dayNumberValue=from.getIntExtra(InputActivity.DAYNUMBER, 0);
-        Double[][] results=calculate_result_arrays(latitudeValue,dayNumberValue);
+        Double[][] summersolstice_results=calculate_result_arrays(latitudeValue,172);
+        Double[][] equinox_results=calculate_result_arrays(latitudeValue,81);
+        Double[][] wintersolistice_results=calculate_result_arrays(latitudeValue,355);
+        Double[] elevation_summersolstice=new Double[summersolstice_results.length];
+        Double[] azimuth_summersolstice=new Double[summersolstice_results.length];
+        Double[] elevation_equinox=new Double[equinox_results.length];
+        Double[] azimuth_equinox=new Double[equinox_results.length];
+        Double[] elevation_wintersolstice=new Double[wintersolistice_results.length];
+        Double[] azimuth_wintersolstice=new Double[wintersolistice_results.length];
+        Double[][] results=calculate_result_arrays(latitudeValue, dayNumberValue);
         Double[] elevation=new Double[results.length];
         Double[] azimuth=new Double[results.length];
-        //results[0] has elevation angle
-        //results[1] has azimuth angle
+
         for (int i=0;i<results.length;i++){
             elevation[i]=results[i][0];
             azimuth[i]=results[i][1];
         }
-       XYSeries series = new SimpleXYSeries(Arrays.asList(azimuth),Arrays.asList(elevation),"Sun Path Diagram");
-           LineAndPointFormatter seriesFormat = new LineAndPointFormatter(Color.rgb(0, 0, 0),null,null,null);
+        for (int i = 0; i <summersolstice_results.length ; i++) {
+            elevation_summersolstice[i]=summersolstice_results[i][0];
+            azimuth_summersolstice[i]=summersolstice_results[i][1];
+        }
+        for (int i = 0; i < wintersolistice_results.length; i++) {
+            elevation_wintersolstice[i]=wintersolistice_results[i][0];
+            azimuth_wintersolstice[i]=wintersolistice_results[i][1];
+        }
+        for (int i = 0; i < equinox_results.length; i++) {
+            elevation_equinox[i]=equinox_results[i][0];
+            azimuth_equinox[i]=equinox_results[i][1];
+        }
+        double minimumxy,sunrise,sunset;
+        int temp_day;
+        if(latitudeValue<0)
+        {
+            temp_day=355;
+
+        }
+        else
+        {
+            temp_day=172;
+        }
+        minimumxy=calculate_time_of_the_day(latitudeValue,temp_day);
+        sunrise=12.0-minimumxy;
+        sunset=12.0+minimumxy;
+        int max_bound=(int)(calculate_elevation_angle(latitudeValue,temp_day,sunrise)[1]+5);
+        int min_bound=(int)(calculate_elevation_angle(latitudeValue,temp_day,sunset)[1]-5);
+        Log.e("min_bound",min_bound +"");
+        Log.e("max_bound",max_bound +"");
+       series = new SimpleXYSeries((Arrays.asList(azimuth)),Arrays.asList(elevation),"User");
+        LineAndPointFormatter seriesFormat = new LineAndPointFormatter(Color.rgb(0, 0, 0),null,null,null);
+        seriesFormat.configure(this,R.xml.line_point_formatter_with_labels);
+        series_summersolstice = new SimpleXYSeries((Arrays.asList(azimuth_summersolstice)),Arrays.asList(elevation_summersolstice),"Summer Solstice");
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.rgb(0, 0, 255),null,null,null);
+        series_wintersolstice = new SimpleXYSeries((Arrays.asList(azimuth_wintersolstice)),Arrays.asList(elevation_wintersolstice),"Winter Solstice");
+        LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.rgb(0, 255, 0),null,null,null);
+        series_eqinox = new SimpleXYSeries((Arrays.asList(azimuth_equinox)),Arrays.asList(elevation_equinox),"Equinox");
+        LineAndPointFormatter series3Format = new LineAndPointFormatter(Color.rgb(255, 0, 0),null,null,null);
+        XYGraphWidget graphWidget=sunpathplot.getGraphWidget();
         sunpathplot.addSeries(series, seriesFormat);
-        sunpathplot.setUserDomainOrigin(0);
-        sunpathplot.setUserRangeOrigin(0);
+        sunpathplot.addSeries(series_eqinox, series3Format);
+        sunpathplot.addSeries(series_summersolstice, series1Format);
+        sunpathplot.addSeries(series_wintersolstice, series2Format);
+        sunpathplot.setDomainBoundaries(-150, 150, BoundaryMode.FIXED);
+        sunpathplot.setRangeBoundaries(0, 90, BoundaryMode.FIXED);
         sunpathplot.setTicksPerDomainLabel(3);
-        sunpathplot.setDomainStep(XYStepMode.INCREMENT_BY_VAL,10);
-        sunpathplot.setRangeStep(XYStepMode.INCREMENT_BY_VAL,5);
-        sunpathplot.getGraphWidget().setPadding(50,0, 0, 50);
+        sunpathplot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, 10);
+        sunpathplot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 5);
+        
+        graphWidget.getGridBackgroundPaint().setARGB(255, 255, 255, 255);
+        graphWidget.getBackgroundPaint().setARGB(255, 255, 255, 255);
+        graphWidget.getDomainTickLabelPaint().setARGB(255,0,0,0);
+        graphWidget.getRangeTickLabelPaint().setARGB(255,0,0,0);
+        graphWidget.getDomainOriginLinePaint().setARGB(255,0,0,0);
+        graphWidget.getRangeOriginLinePaint().setARGB(255,0,0,0);
+        sunpathplot.getBackgroundPaint().setARGB(255, 255, 255, 255);
+        graphWidget.setPadding(50, 25, 30, 50);
+        graphWidget.setSize(new com.androidplot.ui.Size(0.90f, SizeLayoutType.RELATIVE, 1.0f, SizeLayoutType.RELATIVE));
+        graphWidget.position(0.01f, XLayoutStyle.RELATIVE_TO_LEFT, -0.09f, YLayoutStyle.RELATIVE_TO_BOTTOM, AnchorPosition.LEFT_BOTTOM);
+        Paint paint=new Paint();
+        paint.setARGB(255,0,0,0);
+        paint.setTextSize(25);
+        sunpathplot.getLegendWidget().setTextPaint(paint);
+        graphWidget.setRangeValueFormat(new DecimalFormat("#####.##"));
+        graphWidget.setDomainValueFormat(new DecimalFormat("#####.##"));
+        sunpathplot.setRangeLabel("Elevation Angle(Degrees)");
+        sunpathplot.setDomainLabel("Azimuth Angle(Degrees)");
+        sunpathplot.getDomainLabelWidget().setLabelPaint(paint);
+        sunpathplot.getDomainLabelWidget().position(0.0f, XLayoutStyle.RELATIVE_TO_LEFT, -0.075f, YLayoutStyle.RELATIVE_TO_BOTTOM, AnchorPosition.LEFT_BOTTOM);
+        sunpathplot.getDomainLabelWidget().setSize(new com.androidplot.ui.Size(0.05f, SizeLayoutType.RELATIVE, 1.0f, SizeLayoutType.RELATIVE));
+        sunpathplot.getRangeLabelWidget().setLabelPaint(paint);
+        sunpathplot.getRangeLabelWidget().position(0.02f, XLayoutStyle.RELATIVE_TO_LEFT, 0.0f, YLayoutStyle.RELATIVE_TO_BOTTOM, AnchorPosition.LEFT_BOTTOM);
+        sunpathplot.getRangeLabelWidget().setSize(new com.androidplot.ui.Size(1.0f, SizeLayoutType.RELATIVE, 0.05f, SizeLayoutType.RELATIVE));
+        sunpathplot.getLegendWidget().setTableModel(new DynamicTableModel(2, 2));
+        sunpathplot.getLegendWidget().setSize(new com.androidplot.ui.Size(0.075f, SizeLayoutType.RELATIVE, 1.0f, SizeLayoutType.RELATIVE));
+        sunpathplot.getLegendWidget().position(0.1f, XLayoutStyle.RELATIVE_TO_LEFT, -0.008f, YLayoutStyle.RELATIVE_TO_BOTTOM, AnchorPosition.LEFT_BOTTOM);
+//        sunpathplot.setUserDomainOrigin(0);
+//        sunpathplot.setUserRangeOrigin(0);
+//        sunpathplot.setTicksPerDomainLabel(3);
+        sunpathplot.canScrollHorizontally(1);
+        sunpathplot.canScrollVertically(1);
+        sunpathplot.isHorizontalScrollBarEnabled();
+
+//        sunpathplot.redraw();
+        //Set of internal variables for keeping track of the boundaries
+//        sunpathplot.calculateMinMaxVals();
+//        minXY=new PointF(sunpathplot.getCalculatedMinX().floatValue(),sunpathplot.getCalculatedMinY().floatValue());
+//        maxXY=new PointF(sunpathplot.getCalculatedMaxX().floatValue(),sunpathplot.getCalculatedMaxY().floatValue());
     }
 
+
     Double[][]  calculate_result_arrays(double latitude,int day_number){
-        double timeofday=5;
+        double timeofday=calculate_time_of_the_day(latitude,day_number);
+        double sunrise=12.0-timeofday;
+        double sunset=12.0+timeofday;
+        double currentTime=sunrise;
+        Log.e("timeofday",""+timeofday);
+        int a=(int)(timeofday*24);
         int i=0;
-        Double[][] result=new Double[72][2];
-        while(timeofday<17){
-            result[i]=calculate_elevation_angle(latitude,day_number,timeofday);
-            timeofday=timeofday+0.1667;  // incrementing by 1/6
+        Double[][] result=new Double[a+2][2];
+        while(currentTime<sunset+1.0/12.0){
+            result[i]=calculate_elevation_angle(latitude,day_number,currentTime);
+            currentTime=currentTime+1.0/12.0;  // incrementing by 1/6
             i++;
         }
         return result;
@@ -75,7 +185,143 @@ XYPlot sunpathplot;
         }
         else
             azimuth_angle=180-azimuth_angle*360/(2.0*Math.PI);
+        if(azimuth_angle>180)
+            azimuth_angle-=360;
+        else if(azimuth_angle<-180)
+            azimuth_angle+=360;
         elevation_angle=elevation_angle*360/(2.0*Math.PI);
         return new Double[]{elevation_angle,azimuth_angle};
     }
+    public double calculate_time_of_the_day(double latitude,int day_number)
+    {
+        double solar_declination=23.45*(Math.sin(2.0*Math.PI*(day_number-81)/365));
+        double time_of_day=Math.acos(-1.0 * Math.tan(2.0 * Math.PI * latitude / 360) * Math.tan(2.0 * Math.PI * solar_declination / 360));
+        time_of_day=time_of_day*180.0/Math.PI;
+        time_of_day/=15;
+        return  time_of_day;
+    }
+
+
+    //*****************
+    // Definition of the touch states
+    /*static final int NONE = 0;
+    static final int ONE_FINGER_DRAG = 1;
+    static final int TWO_FINGERS_DRAG = 2;
+    int mode = NONE;
+
+    PointF firstFinger;
+    float distBetweenFingers;
+    boolean stopThread = false;
+
+    @Override
+    public boolean onTouch(View arg0, MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: // Start gesture
+                Log.e("mode","ACTION_DOWN");
+                firstFinger = new PointF(event.getX(), event.getY());
+                mode = ONE_FINGER_DRAG;
+                stopThread = true;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_UP:
+                Log.e("mode","Action_pointer_up&&action_up&NONE)");
+                mode = NONE;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN: // second finger
+                Log.e("mode","ACTION_POINTER_DOWN");
+                distBetweenFingers = spacing(event);
+                // the distance check is done to avoid false alarms
+                if (distBetweenFingers > 5f) {
+                    mode = TWO_FINGERS_DRAG;
+                    Log.e("mode","TWO_FINGERS_DRAG");
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == ONE_FINGER_DRAG) {
+                    Log.e("mode","Action_move & mode=ONE_FINGER_DRAG");
+                    PointF oldFirstFinger = firstFinger;
+                    firstFinger = new PointF(event.getX(), event.getY());
+                    Float dist=oldFirstFinger.x - firstFinger.x;
+                    Log.e("dist",""+dist);
+                    if(dist>35)
+                    {
+                        scroll(dist);
+                        sunpathplot.setDomainBoundaries(minXY.x, maxXY.x,
+                                BoundaryMode.FIXED);
+                        sunpathplot.redraw();
+                    }
+                } else if (mode == TWO_FINGERS_DRAG) {
+                    Log.e("mode","Action_move & mode=TWO_fingers_DRAG");
+                    float oldDist = distBetweenFingers;
+                    distBetweenFingers = spacing(event);
+                    zoom(oldDist / distBetweenFingers);
+                    sunpathplot.setDomainBoundaries(minXY.x, maxXY.x,
+                            BoundaryMode.FIXED);
+                    sunpathplot.redraw();
+                }
+                break;
+        }
+        return true;
+    }
+
+    private void zoom(float scale) {
+        float domainSpan = maxXY.x - minXY.x;
+        float oldMax = maxXY.x;
+        float oldMin = minXY.x;
+        float domainMidPoint = maxXY.x - (domainSpan / 2.0f);
+        float offset = domainSpan * scale / 2.0f;
+        minXY.x = domainMidPoint - offset;
+        maxXY.x = domainMidPoint + offset;
+        float newSpan = maxXY.x - minXY.x;
+        if (newSpan < (float)5) {
+            minXY.x = oldMin;
+            maxXY.x = oldMax;
+        }
+
+        if (minXY.x < leftBoundary) {
+            minXY.x = leftBoundary;
+            maxXY.x = leftBoundary + domainSpan * zoomRatio;
+
+           try {
+               if (maxXY.x > series.getX(series.size() - 1).floatValue())
+                   maxXY.x = rightBoundary;
+           }catch (NullPointerException e){
+               Log.e("error",e.toString());
+               e.printStackTrace();
+           }
+        }
+        Log.e("size",series.size()+"");
+        if (maxXY.x > series.getX(series.size() - 1).floatValue()) {
+            maxXY.x = rightBoundary;
+            minXY.x = rightBoundary - domainSpan * zoomRatio;
+            if (minXY.x < leftBoundary) minXY.x = leftBoundary;
+        }
+    }
+
+    private void scroll(float pan) {
+        float domainSpan = maxXY.x - minXY.x;
+        float step = domainSpan / sunpathplot.getWidth();
+        float offset = pan * step;
+        minXY.x = minXY.x + offset;
+        maxXY.x = maxXY.x + offset;
+
+        if (minXY.x < leftBoundary) {
+            minXY.x = leftBoundary;
+            maxXY.x = leftBoundary + domainSpan;
+        }
+        Log.e("size",series.size()+"");
+        Log.e("a","afsa");
+        if (maxXY.x > series.getX(series.size() - 1).floatValue()) {
+            maxXY.x = rightBoundary;
+            minXY.x = rightBoundary - domainSpan;
+        }
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float)Math.sqrt(x * x + y * y);
+    }
+*/
+
 }
